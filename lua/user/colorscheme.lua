05e9b0fb1d713196119aca3fbdc0fd2facf57aab
Tiago <tiagohs.dev@gmail.com>
2022-07-13 17:11:30 -0300

    add corretor pt-Br

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,4 +1,22 @@
-local colorscheme = "darkplus"
+-- local colorscheme = "darkplus"
+local colorscheme = "gruvbox-material"
+vim.g.gruvbox_material_background = "hard"
+vim.g.gruvbox_material_disable_italic_comment = 1
+vim.g.gruvbox_material_enable_bold = 0
+vim.g.gruvbox_material_enable_italic = 0
+vim.g.gruvbox_material_transparent_background = 0
+vim.g.gruvbox_material_visual = "grey background"
+vim.g.gruvbox_material_menu_selection_background = "grey"
+vim.g.gruvbox_material_sign_column_background = "default"
+vim.g.gruvbox_material_spell_foreground = "colored"
+vim.g.gruvbox_material_ui_contrast = "low"
+vim.g.gruvbox_material_diagnostic_text_highlight = 1
+vim.g.gruvbox_material_diagnostic_line_highlight = 1
+vim.g.gruvbox_material_diagnostic_virtual_text = "gray"
+vim.g.gruvbox_material_current_word = "grey background"
+vim.g.gruvbox_material_statusline_style = "default"
+vim.g.gruvbox_material_lightline_disable_bold = 0
+vim.g.gruvbox_material_palette = "material"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
